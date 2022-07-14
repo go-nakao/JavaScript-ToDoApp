b7af2623bb0db39c210ac8b3cd89868c17c72af9
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-07-14 22:22:49 +0900

    add-text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import "./styles.css";
+// 入力エリア機能実装
+const onClickAdd = () => {
+  const getText = document.getElementById("add-text").value; //テキストボックスの値を取得
+  document.getElementById("add-text").value = "";  //登録ボタン押されたら、テキストボックスを初期化
+
+  // divを生成
+  const div = document.createElement("div");
+
+  //クラス名を付与
+  div.className = "list-row";
+
+  //liタグを生成
+  const li = document.createElement("li");
+  //liタグに要素を格納する
+  li.innerText = getText;
+
+  // divタグの子要素にliタグを設定
+  div.appendChild(li);
+
+  //Todo一覧に追加
+  document.getElementById("index-todo").appendChild(div);
+}
+
+// add-buttonというidに対して、clickイベントを追加する
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());

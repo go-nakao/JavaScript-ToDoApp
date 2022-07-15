72278f534ab8206c6f36a8bf160dc8901502099f
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-07-15 13:39:22 +0900

    完了ボタンが押された際にTodo一覧から削除

--- src/index.js
+++ src/index.js
@@ -14,8 +14,30 @@ const onClickAdd = () => {
   //liタグに要素を格納する
   li.innerText = getText;
 
+  //ボタン(完了)を生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了しました")
+    //完了ボタンを押すと完了済エリアにTodoが移動し、Todo一覧からは削除される。
+    //完了済エリアにDOMを生成する
+    const deleteTarget = completeButton.parentNode;
+    document.getElementById("index-todo").removeChild(deleteTarget);
+  });
+
+  //削除ボタンを生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親親タグ(div)をTodoリストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("index-todo").removeChild(deleteTarget);
+  });
   // divタグの子要素にliタグを設定
   div.appendChild(li);
+  // divタグの下にbuttonを配置
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //Todo一覧に追加
   document.getElementById("index-todo").appendChild(div);
